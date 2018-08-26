6bf8fb2722f65145c9bbc3d4f74c5d85519ccd99
Sgryt <sgryt@Sgryt.local>
2018-08-26 18:24:32 -0500

    styling updated

--- app.py
+++ app.py
@@ -1,17 +1,34 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
 
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Author1',
+        'title': 'title1',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae consectetur hic molestias? Ab blanditiis cupiditate debitis dicta doloribus eaque esse expedita laborum libero nam neque officia quo, saepe soluta, vero.',
+        'date_posted': 'Jan 1, 2000'
+    },
+
+    {
+        'author': 'Author2',
+        'title': 'title2',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae consectetur hic molestias? Ab blanditiis cupiditate debitis dicta doloribus eaque esse expedita laborum libero nam neque officia quo, saepe soluta, vero.',
+        'date_posted': 'Jan 2, 2002'
+    }
+
+]
 
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return '<h1>About Page</h1>'
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
