e71f9d79ef45175ff537f30040a4de73f42e8c31
Sgryt <sgryt@Sgryt.local>
2018-08-26 21:59:35 -0500

    registration validation added

--- app.py
+++ app.py
@@ -1,34 +1,55 @@
-from flask import Flask, request, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 posts = [
     {
         'author': 'Author1',
-        'title': 'title1',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae consectetur hic molestias? Ab blanditiis cupiditate debitis dicta doloribus eaque esse expedita laborum libero nam neque officia quo, saepe soluta, vero.',
+        'title': 'Title1',
+        'content': '   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae commodi doloremque doloribus earum eveniet facere incidunt ipsam, magnam minus, nemo nihil, praesentium qui quidem recusandae repellendus rerum sequi totam voluptatibus!',
         'date_posted': 'Jan 1, 2000'
     },
-
     {
         'author': 'Author2',
-        'title': 'title2',
-        'content': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae consectetur hic molestias? Ab blanditiis cupiditate debitis dicta doloribus eaque esse expedita laborum libero nam neque officia quo, saepe soluta, vero.',
+        'title': 'Title2',
+        'content': '   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae commodi doloremque doloribus earum eveniet facere incidunt ipsam, magnam minus, nemo nihil, praesentium qui quidem recusandae repellendus rerum sequi totam voluptatibus!',
         'date_posted': 'Jan 2, 2002'
     }
-
 ]
 
 
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
 
-@app.route('/about')
+@app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
