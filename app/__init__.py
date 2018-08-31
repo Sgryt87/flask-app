10703e8a5264d4b575237b3e22d649ec3be8039a
Sgryt87 <sergiigrytsaienko@gmail.com>
2018-08-30 19:02:07 -0500

    error handling&custom error pages are added

--- app/__init__.py
+++ app/__init__.py
@@ -21,12 +21,15 @@ def create_app(config_class=Config):
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
+
     from app.users.routes import users
     from app.posts.routes import posts
     from app.main.routes import main
+    from app.errors.handlers import errors
 
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
+    app.register_blueprint(errors)
 
     return app
